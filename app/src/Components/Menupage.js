c3cc5c9ec94d28c6b149a997cd7e2e887a606e72
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-03-09 21:04:04 +0000

    added pictures

--- app/src/Components/Menupage.js
+++ app/src/Components/Menupage.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import getData from "./Utilities/Data";
 
 function Menupage() {
-  const API = `https://www.jsonkeeper.com/b/MDXW`;
+  const API = `https://www.jsonkeeper.com/b/HEP6`;
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -30,18 +30,22 @@ function Menupage() {
     <>
       <Navigation />
       <div className="container">
-      <div className="d-flex row">
-        {items.map((item) => (
-          <div key={item.id} className="card col-4 " style={{ width: "18rem" }}>
-            <img src={item.image} className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">{item.title}</h5>
-              <p className="card-text">{item.description}</p>
-              <p className="card-text">{item.price}</p>
+        <div className="d-flex row">
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="card col-4 justify-content-center intro ign-items-center py-5"
+              style={{ width: "18rem" }}
+            >
+              <img src={item.image} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.description}</p>
+                <p className="card-text">{item.price}</p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </>
