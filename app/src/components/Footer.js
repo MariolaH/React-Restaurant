5c249556021b970c5b548d607c17e5591cba2078
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-30 18:23:46 +0000

    responsive updates

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -7,9 +7,10 @@ function Footer() {
         <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top border-danger border-2">
           <div className="col-4 mb-3">
             <img
-              src="./IMAGE/Screen Shot 2023-03-08 at 9.05.56 PM.png"
-              alt="react logo"
-              style={{ width: "400px" }}
+            className="map"
+              src="./IMAGE/map.png"
+              alt="map"
+              // style={{ width: "400px" }}
             />
 
             <a href="https://www.google.com/maps/place/348+E+Main+St,+Lexington,+KY+40507/@38.0421746,-84.4947276,17z/data=!3m1!4b1!4m6!3m5!1s0x884244e844532c33:0xb6919957af36b713!8m2!3d38.0421704!4d-84.4925389!16s%2Fg%2F11c5n2lnvk">
@@ -93,3 +94,98 @@ function Footer() {
 }
 
 export default Footer;
+
+// import React from "react";
+
+// function Footer() {
+//   return (
+//     <div className="container">
+//       <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top border-danger border-2">
+//         <div className="col-12 col-sm-4 mb-3">
+//           <img
+//             className="map img-fluid"
+//             src="./IMAGE/map.png"
+//             alt="map"
+//           />
+
+          
+//           <a
+//             href="https://www.google.com/maps/place/348+E+Main+St,+Lexington,+KY+40507/@38.0421746,-84.4947276,17z/data=!3m1!4b1!4m6!3m5!1s0x884244e844532c33:0xb6919957af36b713!8m2!3d38.0421704!4d-84.4925389!16s%2Fg%2F11c5n2lnvk"
+//             target="_blank"
+//             rel="noreferrer"
+//           >
+//             <svg className="bi me-2" width="40" height="32"></svg>
+//           </a>
+//           <div className="d-grid gap-2 p-2">
+//             <a
+//               href="https://www.opentable.com/"
+//               target="_blank"
+//               rel="noreferrer"
+//             >
+//               <button className="btn btn-outline-danger btn-lg" type="button">
+//                 MAKE A RESERVATION
+//               </button>
+//             </a>
+//             <div className="p-2"></div>
+//             <button type="button" className="btn btn-outline-danger btn-lg">
+//               EMAIL US!
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="col-12 col-sm-4 mb-3"></div>
+
+//         <div className="col-12 col-sm-4 mb-3">
+//           <h3>
+//             <b>A D D R E S S </b>
+//           </h3>
+//           <br />
+//           <ul className="nav flex-column">
+//             <li className="nav-item mb-2">
+//               <h6>348 E Main Street</h6>
+//             </li>
+//             <li className="nav-item mb-2">
+//               <h6>Lexington, Kentucky</h6>
+//             </li>
+//             <li className="nav-item mb-2">
+//               <h6>40507</h6>
+//             </li>
+//           </ul>
+//         </div>
+
+//         <div className="col-12 col-sm-4 mb-3">
+//           <h3>
+//             <b>H O U R S </b>
+//           </h3>
+//           <br />
+//           <ul className="nav flex-column">
+//             <li className="nav-item mb-2">
+//               <h6>Sunday: 5 PM - 10 PM</h6>
+//             </li>
+//             <li className="nav-item mb-2">
+//               <h6>Monday: 5 PM - 10 PM</h6>
+//             </li>
+//             <li className="nav-item mb-2">
+//               <h6>Tuesday: 5 PM - 10 PM</h6>
+//             </li>
+//             <li className="nav-item mb-2">
+//               <h6>Wednesday: 5 PM - 11 PM</h6>
+//             </li>
+//             <li className="nav-item mb-2">
+//               <h6>Thursday: 5 PM-11PM</h6>
+//             </li>
+//                <li className="nav-item mb-2">
+//                 <h6>Friday: 5 PM - 12 PM</h6>
+//               </li>
+//               <li className="nav-item mb-2">
+//                 <h6>Saturday: 5 PM - 12 PM</h6>
+//               </li>
+//             </ul>
+//           </div>
+//         </footer>
+//       </div>
+//   );
+// }
+
+// export default Footer;
+
--- app/src/components/Mainpage.js
+++ app/src/components/Mainpage.js
@@ -9,7 +9,7 @@ function Mainpage() {
       <div className="frame">
         <div className="container">
           <div className="first row">
-            <div className=" headerimage col d-flex justify-content-center">
+            <div className="headerimage col d-flex justify-content-center">
               <img src="./IMAGE/restaurant1.jpeg" alt="Mona Lisa" />
             </div>
           </div>
@@ -24,8 +24,8 @@ function Mainpage() {
           </div>
 
           <div className="third row">
-            <div className="col-2"></div>
-            <div className="col-6 ">
+            <div className="col-md-2"></div>
+            <div className="col-md-6 ">
               <h3>ABOUT US</h3>
               <p className="bio">
                 The roots of our Italian restaurant date back to 1930, when it
@@ -40,8 +40,19 @@ function Mainpage() {
                 heritage of Italian cuisine.
               </p>
             </div>
-            <div className="col-4">
+            {/* d-md-block d-none hides it on mobile devices */}
+            <div className="col-md-4 d-md-block d-none">
               {" "}
+              <img
+                src="./IMAGE/Mona Lisa.jpeg"
+                className="monaLisa"
+                alt="Mona Lisa"
+              />
+            </div>
+          </div>
+
+          <div className="row d-md-none">
+            <div className="col-12">
               <img
                 src="./IMAGE/Mona Lisa.jpeg"
                 className="monaLisa"
