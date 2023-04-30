4b75a500014e201c4a3a93d87dc728ff09eda2f3
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-30 13:57:16 +0000

    changed navbar

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -6,20 +6,25 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 function Navigation() {
   return (
     <>
-      <Navbar className="navbar mb-5">
+      <Navbar className="navbar mb-5" expand="md">
         <Container>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/"}>
-              <b> H O M E </b> &#160; &#160; &#160;
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/menupage"}>
-              M E N U &#160; &#160;
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/drinks"}>
-              C O C K T A I L S &#160; &#160;
-            </Nav.Link>
-          </Nav>
-          <h1>Mona Lisa Bistro</h1>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to={"/"}>
+                <b> H O M E </b> &#160; &#160; &#160;
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/menupage"}>
+                M E N U &#160; &#160;
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/drinks"}>
+                C O C K T A I L S &#160; &#160;
+              </Nav.Link>
+            </Nav>
+            <Navbar.Brand className="ms-auto">
+              <h1>Mona Lisa Bistro</h1>
+            </Navbar.Brand>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
